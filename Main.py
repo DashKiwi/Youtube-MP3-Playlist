319a1c80b7ce7423ee9680ddd177916d90acfab6
DashKiwi <Maxmooney08@gmail.com>
2024-08-06 08:44:03 +1200

    Adds info to json and is able to read from it
    Made it so that when you create a new playlist, inside the json file it adds a new block with the name of the playlist. Inside that block there is a single object called song_count.
    
    Made it so that in the main menu it displays all of the playlists that you currently have which is grabbed from the json file. When you choose one of the playlists it takes you to the corrisponding playlist menu
    
    Inside the playlist menu it now displays all the songs that a playlist has underneath it. It does this by grabbing them from the json file. Currently when you choose the song it does not play.
    
    To do:
    Make it so when you choose a song from a playlist it plays
    Make you able to delete playlists in the main menu
    Make it possible to remove songs, skip songs, rename the playlist and toggle shuffling in the playlist menu

--- Main.py
+++ Main.py
@@ -9,43 +9,62 @@ from pydub import AudioSegment, playback
 playlist = ""
 
 # Functions
-def new_download():
+
+def new_playlist():
     global playlist
-    # gets the wanted link
-    vid_link = str(input("Enter the URL of the video you want to download:\n"))
-    # Gets the video
-    yt = YouTube(vid_link, on_progress_callback=on_progress)
-    # Chooses where to store
-    destination = ".\Music\{}".format(playlist)
-    # Downloads the video
-    video = yt.streams.get_audio_only()
-    video.download(mp3=True, output_path=destination) # pass the parameter mp3=True to save in .mp3
-    # Says when its been downloaded
+    # clears the console
+    os.system('cls')
     while True:
-        choice = string_checker(yt.title + " has been successfully downloaded. enter e to return to Playlist menu")
+        # Gets what you want to name the playlist
+        playlist = input("Name Of the playlist: ")
+        # checks if the playlist path already exists, if it doesnt it creates a new folder for the playlist
+        if not os.path.isdir(r".\Music\{}".format(playlist)):
+            os.makedirs(r".\Music\{}".format(playlist))
+            # adds the playlist to the json file
+            new_playlist_name = {playlist: {
+                "song_count": 0
+                }
+            }
+            # reads the file and gets all the data
+            save_file = open(r".\Music\Playlists.json", "r")
+            data = dict(json.load(save_file))
+            save_file.close()
+            # adds the new playlist to the json file
+            data.update(new_playlist_name)
+            print(data)
+            # gains write acces to the json file
+            save_file = open(r".\Music\Playlists.json", "w")
+            # rewrites the json file with all the data
+            json.dump(data, save_file, indent=6)
+            # saves the file
+            save_file.close()
+            # moves to the playlist menu
+            playlist_menu()
+        else:
+            print("A Playlist with this name already exists\n")
+    
+
+def new_song():
+    os.system('cls')
+    print("Add song to {}\n".format(playlist))
+    vid_link = str(input("\nYoutube Link: "))
+    yt = YouTube(vid_link, on_progress_callback=on_progress)
+    name = input("\nSong Name (Leave Blank for video name): ") or yt.title
+    yt.title = name
+    video = yt.streams.get_audio_only()
+    video.download(mp3=True, output_path=".\Music\{}".format(playlist)) # pass the parameter mp3=True to save in .mp3
+    save_file = open(r".\Music\Playlists.json", "r")
+    data = dict(json.load(save_file))
+    save_file.close()
+    data[playlist]["song_count"] += 1
+    data[playlist]["song {}".format(data[playlist]["song_count"])] = yt.title
+    save_file = open(r".\Music\Playlists.json", "w")
+    json.dump(data, save_file, indent=6)
+    save_file.close()
+    while True:
+        choice = input("{} Has been successfully downloaded type e it return to playlist menu: ".format(yt.title)).capitalize()
         if choice == "E":
             playlist_menu()
-            break
-
-def string_checker(prompt):
-    while True:
-        try:
-            return(str(input(prompt).capitalize()))
-        except:
-            print("Please enter a valid request")
-
-def start():
-    usr_choice = int(input("Do you want to\n1. Download new music\n2. listen to downloaded music\n>>"))
-    if usr_choice == 1:
-        new_song()
-    elif usr_choice == 2:
-        Music = AudioSegment.from_file(".\Music\Arms Shouldn't Look Like This.mp3", format="mp3")
-        play_obj = simpleaudio.WaveObject(Music.raw_data, num_channels= Music.channels, bytes_per_sample= Music.sample_width, sample_rate= Music.frame_rate)
-        playing = play_obj.play()
-        choice = input("DaDitDaDit")
-        if choice == "a":
-            playing.stop()
-        playing.wait_done()
 
 def playlist_menu():
     global playlist
@@ -57,84 +76,75 @@ def playlist_menu():
     data = json.load(save_file)
     # Displays all the songs in the play list
     num = 0
-    for j in data[playlist]:
+    for i in data[playlist]:
         if num != 0:
-            num += 1
-            print("{}) ".format(num) + data[playlist][j])
+            print("{}) ".format(num) + data[playlist][i])
+        num += 1
     # choose what the user wants to play/do
-    choice = string_checker("\na) New Song\nd) Remove Song\ns) skip current song\nr) rename playlist\nt) toggle shuffle (ON)\ne) Main Menu")
-    # checks all the user choices
-    if choice == "A":
-        new_download()
-
-def new_playlist():
-    global playlist
-    # clears the console
-    os.system('cls')
-    # Gets what you want to name the playlist
-    playlist = input("Name Of the playlist: ")
-    # adds the playlist to the json file
-    new_playlist_name = {playlist: {
-        "song_count": "0"
-    }}
-    # reads the file and gets all the data
-    save_file = open(r".\Music\Playlists.json", "r")
-    data = dict(json.load(save_file))
-    save_file.close()
-    # adds the new playlist to the json file
-    data.update(new_playlist_name)
-    print(data)
-    # gains write acces to the json file
-    save_file = open(r".\Music\Playlists.json", "w")
-    # rewrites the json file with all the data
-    json.dump(data, save_file, indent=6)
-    # saves the file
-    save_file.close()
-    # checks if the playlist path already exists, if it doesnt it creates a new folder for the playlist
-    if not os.path.isdir(r".\Music\{}".format(playlist)):
-        os.makedirs(r".\Music\{}".format(playlist))
-    # moves to the playlist menu
-    playlist_menu()
-    
-
-def new_song():
-    os.system('cls')
-    print("Add song to {}\n".format(playlist))
-    vid_link = str(input("\nYoutube Link: "))
-    yt = YouTube(vid_link, on_progress_callback=on_progress)
-    yt.title = string_checker("\nSong Name: ")
-    video = yt.streams.get_audio_only()
-    video.download(mp3=True, output_path=".\Music\{}".format(playlist)) # pass the parameter mp3=True to save in .mp3
-    save_file = open(r".\Music\Playlists.json", "r")
-    data = dict(json.load(save_file))
-    save_file.close()
-    data[playlist]["song_count"] += 1
-    data[playlist]["song{}".format(data[playlist]["song_count"])] = yt.title
-    save_file = open(r".\Music\Playlists.json", "w")
-    json.dump(data, save_file, indent=6)
-    save_file.close()
-
     while True:
-        choice = string_checker("{} Has been successfully downloaded type e it return to playlist menu".format(yt.title))
-        if choice == "E":
-            playlist_menu()
-        
+        choice = input("\na) New Song\nd) Remove Song\ns) skip current song\nr) rename playlist\nt) toggle shuffle (ON)\ne) Main Menu\n").capitalize()
+        try:
+            choice = eval(choice)
+            if isinstance(choice, int):
+                    save_file = open(r".\Music\Playlists.json", "r")
+                    data = dict(json.load(save_file))
+                    save_file.close()
+                    n = 0
+                    for i in data:
+                        n += 1
+                        if n == int(choice):
+                            print("Yay its working")
+                    # Put the code you want to run the music here
+        except:
+            # checks all the user choices
+            if choice == "A":
+                new_song()
+            elif choice == "D":
+                print("ADD REMOVE SONG")
+            elif choice == "S":
+                print("ADD SKIPPING SONGS!")
+            elif choice == "R":
+                print("ADD RENAMING PLAYLISTS")
+            elif choice == "T":
+                print("ADD TOGGLE SHUFFLING")
+            elif choice == "E":
+                main_menu()
 
 def main_menu():
-    os.system('cls')
-    save_file = open(r".\Music\Playlists.json", "r")
-    data = json.load(save_file)
-    num = 0
-    for i in data:
-        num += 1
-        print("{}) ".format(num) + data[i])
-    choice = string_checker("_Open Music App_\n\n1) #Test Start\n2) #playlist b\n3) #playlist c\n\nc) New Playlist\nX) Exit\n")
-    if choice == "1" or choice == "2" or choice == "3":
-        start()
-    elif choice == "C":
-        new_playlist()
-    elif choice == "X":
-        os.close
+        global playlist
+        os.system('cls')
+        print("_Open Music App_\n\n")
+        save_file = open(r".\Music\Playlists.json", "r")
+        data = json.load(save_file)
+        num = 0
+        display = ""
+        for i in data:
+            num += 1
+            display += "{}) {}\n".format(num, i)
+        while True:
+            choice = input("{}\nc) New Playlist\nX) Exit\n".format(display)).capitalize()
+            try:
+                choice = eval(choice)
+                if isinstance(choice, int):
+                    save_file = open(r".\Music\Playlists.json", "r")
+                    data = dict(json.load(save_file))
+                    save_file.close()
+                    n = 0
+                    for i in data:
+                        n += 1
+                        print(choice)
+                        if n == int(choice):
+                            playlist = i
+                    print(playlist)
+                    playlist_menu()
+                    break
+            except:
+                if choice == "C":
+                    new_playlist()
+                elif choice == "X":
+                    os.close
+                else:
+                    print("Please enter a valid option")
 
 # Main Process
 os.system('cls')

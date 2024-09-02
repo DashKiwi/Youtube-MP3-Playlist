df1090746483ccd2433a6187a23d19ef1d072458
DashKiwi <Maxmooney08@gmail.com>
2024-09-02 20:59:11 +1200

    Added an automatic ffmpeg installation.
    Inside the setup.bat file it opens a powershell script to install ffmpeg automatically using choco. As far as I believe this program now works to install ffmpeg when the user runs the setup shortcut.
    
    Added a shortcut that asks for administrator purposes. This is to allow the ffmpeg be installed.

--- Setup Files/Setup.bat
+++ Setup Files/Setup.bat
@@ -2,4 +2,4 @@ pip install pytubefix
 pip install wheel
 pip install pydub
 pip install simpleaudio
-Powershell.exe -executionpolicy remotesigned -File  %CD%\ffmpeg-requirements.ps1
+Powershell.exe -executionpolicy remotesigned -File "%~dp0ffmpeg-requirements.ps1"

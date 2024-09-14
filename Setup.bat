a2839d0227a22f3a382af697f5f00c3eb356041f
DashKiwi <Maxmooney08@gmail.com>
2024-09-15 10:02:13 +1200

    Changed how the setup bat file works
    Added a script in the setup.bat file that asks for administrator rights. This is so I no longer need a shortcut because that was previously causing problems

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,10 @@
+@echo off
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+@echo on
+
 pip install pytubefix
 pip install wheel
 pip install pydub
